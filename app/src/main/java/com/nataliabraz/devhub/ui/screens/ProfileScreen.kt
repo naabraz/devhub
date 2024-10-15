00882fa3b5ad6e174f76134598b71d81e932dad4
Natalia Braz <naabraz@gmail.com>
2024-10-15 09:10:01 -0300

    fix: adjust state after navigation

--- app/src/main/java/com/nataliabraz/devhub/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/nataliabraz/devhub/ui/screens/ProfileScreen.kt
@@ -1,24 +1,22 @@
 package com.nataliabraz.devhub.ui.screens
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import com.nataliabraz.devhub.ui.components.Profile
 import com.nataliabraz.devhub.webclient.GithubWebClient
 
 @Composable
 fun ProfileScreen(
-    id: String,
-    webClient: GithubWebClient
+    id: String, webClient: GithubWebClient
 ) {
-    Log.i("ProfileScreen", "ProfileScreen: $id")
-    val uiState = webClient.uiState
+    val uiState by remember { derivedStateOf { webClient.uiState } }
 
     LaunchedEffect(null) {
         webClient.getUserProfile(id)
     }
-
-    Log.i("ProfileScreen", "ProfileScreen: $uiState")
 
     Profile(uiState)
 }
